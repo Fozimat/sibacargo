2309aa4ef8c8c5eb21d55b2399ae125accc6f84d
Fozimat <fozimata@gmail.com>
2022-05-20 08:59:33 +0700

    pesan masuk ok

--- app/Http/Controllers/Admin/PesanController.php
+++ app/Http/Controllers/Admin/PesanController.php
@@ -80,8 +80,9 @@ class PesanController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Pesan $pesan)
     {
-        //
+        $pesan->delete();
+        return redirect()->route('pesan.index')->with('flash', 'Pesan berhasil dihapus');
     }
 }
